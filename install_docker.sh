36a2e4475a24d8c4e4f99bdae5b54179c2586ca3
chalut01 <jomzsg@hotmail.com>
2018-06-06 11:32:21 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository -y \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update -y
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce --allow-unauthenticated
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
