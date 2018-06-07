1663f59a0ef7be863a268b91a17a6ea8bc3e9058
chalut01 <jomzsg@hotmail.com>
2018-06-07 14:33:50 +0700

    Update setuphost.sh

--- setuphost.sh
+++ setuphost.sh
@@ -8,6 +8,10 @@ gateway=$3
 ## hosts
 echo $hostname 
 
+wget https://raw.githubusercontent.com/chalut01/setup/master/hostname
+wget https://raw.githubusercontent.com/chalut01/setup/master/hosts
+wget https://raw.githubusercontent.com/chalut01/setup/master/interfaces
+
 sed -i 's/{{hostname}}/$hostname/g' hostname
 sed -i 's/{{hostname}}/$hostname/g' hosts
 sed -i 's/{{ip}}/$ip/g' interfaces
