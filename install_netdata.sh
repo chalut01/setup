c5ae91a6a70431c342966c14e5eef923e2783960
Chalut Dumrongsiri <jomzsg@hotmail.com>
2018-08-21 15:14:46 +0700

    Update install_netdata.sh

--- install_netdata.sh
+++ install_netdata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #prepare
 
+apt-get update
+
 curl -Ss 'https://raw.githubusercontent.com/firehol/netdata-demo-site/master/install-required-packages.sh' >/tmp/kickstart.sh && bash /tmp/kickstart.sh -i netdata-all
 
 git clone https://github.com/firehol/netdata.git --depth=1
