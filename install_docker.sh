bf3e02102bd6e7b01c0c1b323ef352669522171f
Chalut Dumrongsiri <jomzsg@hotmail.com>
2018-09-14 10:54:05 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -14,3 +14,4 @@ sudo apt-get install -y docker-ce --allow-unauthenticated
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+apt-get install -y docker.io 
