95e28f0ff957048a5aaf82e31c0dffc4576c1119
Chalut Dumrongsiri <jomzsg@hotmail.com>
2019-10-18 15:10:53 +0700

    Create install_docker_ubuntu18.sh

---
+++ install_docker_ubuntu18.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
