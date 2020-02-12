db43e0ce7f20f11cc701d369af8600ef017f8d25
Chalut Dumrongsiri <jomzsg@hotmail.com>
2020-02-12 09:22:50 +0700

    Update install_netdata.sh

--- install_netdata.sh
+++ install_netdata.sh
@@ -18,7 +18,7 @@ echo "1. apt-get install -y libyajl-dev libaprutil1 libc6 nginx"
 echo "2. service nginx stop"
 echo "3. systemctl disable nginx"
 echo "4. cd /opt/stack/script"
-echo "5. edit nginx.chat.sh ,  nginx.tcp.sh , nginx.wildfly.sh" 
+echo "5. edit nginx.chat.sh ,  nginx.tcp.sh , nginx.wildfly.sh , ctlscript.sh" 
 echo "6. comment line export LD_LIBRARY_PATH='/opt/stack/nginx/lib/:/opt/stack/mqtt/lib/' "
 echo "7. restart nginx"
 
