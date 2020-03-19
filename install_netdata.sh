2cbcee576efdc658fa3217484474a64bd1f39f83
Chalut Dumrongsiri <jomzsg@hotmail.com>
2020-03-19 10:13:46 +0700

    install require

--- install_netdata.sh
+++ install_netdata.sh
@@ -2,6 +2,7 @@
 #prepare
 
 ##apt-get update -y
+apt-get install -y autoconf autoconf-archive autogen automake curl gcc git gzip libjudy-dev liblz4-dev libmnl-dev libssl-dev libuv1-dev lm-sensors make netcat nodejs pkg-config python python-mysqldb python-psycopg2 python-pymongo tar uuid-dev zlib1g-dev 
 
 curl -Ss 'https://raw.githubusercontent.com/firehol/netdata-demo-site/master/install-required-packages.sh' >/tmp/kickstart.sh && bash /tmp/kickstart.sh -i netdata-all
 
