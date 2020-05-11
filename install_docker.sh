edad6ab481ef524d028d6f4478baa491b8d16e58
Chalut Dumrongsiri <jomzsg@hotmail.com>
2020-05-11 09:46:20 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository -y \
    stable"
 sudo apt-get update -y
 sudo apt-get install -y docker-ce --allow-unauthenticated
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 apt-get install -y docker.io 
