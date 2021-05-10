dbb01e340032d8c41fab47ff494a2d0bf33d6119
Chalut Dumrongsiri <jomzsg@hotmail.com>
2021-05-10 15:31:05 +0700

    Update addRepoSuse11.4.sh

--- addRepoSuse11.4.sh
+++ addRepoSuse11.4.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 zypper ar -c -g -f http://ftp5.gwdg.de/pub/opensuse/discontinued/distribution/11.4/repo/oss repo1-oss
+zypper ar -c -g -f http://ftp5.gwdg.de/pub/opensuse/discontinued/distribution/12.3/repo/oss/ oss-12
+zypper ar -c -g -f http://ftp5.gwdg.de/pub/opensuse/discontinued/distribution/13.2/repo/oss/  oss-13
+zypper ar -c -g -f http://ftp5.gwdg.de/pub/opensuse/discontinued/distribution/leap/15.0/repo/oss/ oss-15
 sudo zypper refs
 sudo zypper ref
-sudo zypper up
+
+#sudo zypper up
