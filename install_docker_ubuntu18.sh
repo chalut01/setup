6eadfae541117e8d28b948ccd4dce5e4692ff8b4
Chalut Dumrongsiri <jomzsg@hotmail.com>
2021-09-15 09:27:19 +0700

    Update install_docker_ubuntu18.sh

--- install_docker_ubuntu18.sh
+++ install_docker_ubuntu18.sh
@@ -3,9 +3,9 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce
+apt install -y docker-ce
