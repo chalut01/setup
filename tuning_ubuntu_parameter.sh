00dde43374613d22dbe7511be7ffe66157061bf8
Chalut Dumrongsiri <jomzsg@hotmail.com>
2022-03-27 16:07:54 +0700

    Update tuning_ubuntu_parameter.sh

--- tuning_ubuntu_parameter.sh
+++ tuning_ubuntu_parameter.sh
@@ -8,5 +8,5 @@ echo "*         soft    nofile      65535" >> /etc/security/limits.conf
 ulimit -SHn 65535
 
 # sysctl tuning
-curl -o /etc/sysctl.conf https://gist.githubusercontent.com/yegorg/36cf9710e8ef50fa07571db0f4b981f9/raw/b82710dfd03df131818ced7634c1c7b7e663dc6c/sysctl.conf
+curl -o /etc/sysctl.conf https://raw.githubusercontent.com/chalut01/setup/master/template/sysctl.conf
 sysctl -e -p /etc/sysctl.conf
