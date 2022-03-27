08409421ff50966e2b4380f9d0b5802e88842251
Chalut Dumrongsiri <jomzsg@hotmail.com>
2022-03-27 15:59:25 +0700

    Update install_netdata.sh

--- install_netdata.sh
+++ install_netdata.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-#prepare
 
-##apt-get update -y
-apt-get install -y autoconf autoconf-archive autogen automake curl gcc git gzip libjudy-dev liblz4-dev libmnl-dev libssl-dev libuv1-dev lm-sensors make netcat nodejs pkg-config python python-mysqldb python-psycopg2 python-pymongo tar uuid-dev zlib1g-dev 
+apt-get update -y
 
-curl -Ss 'https://raw.githubusercontent.com/firehol/netdata-demo-site/master/install-required-packages.sh' >/tmp/kickstart.sh && bash /tmp/kickstart.sh -i netdata-all
-
-git clone https://github.com/firehol/netdata.git --depth=1
-cd netdata
-
-./netdata-installer.sh --install /opt/stack
-
-echo ""
-echo "" 
-echo ""
-echo "If install in DMA...... Do the following step"
-echo "1. apt-get install -y libyajl-dev libaprutil1 libc6 nginx"
-echo "2. service nginx stop"
-echo "3. systemctl disable nginx"
-echo "4. cd /opt/stack/script"
-echo "5. edit nginx.chat.sh ,  nginx.tcp.sh , nginx.wildfly.sh , ctlscript.sh" 
-echo "6. comment line export LD_LIBRARY_PATH='/opt/stack/nginx/lib/:/opt/stack/mqtt/lib/' "
-echo "7. FINISH"
-
+# install netdata
+wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh
+sleep 3
+# Tuning netdata configuration 
+curl -o /etc/netdata/netdata.conf http://localhost:19999/netdata.conf
+sed -i 's/history = 3996/history = 608400/g' /etc/netdata/netdata.conf
+sed -i 's/# dbengine disk space = 256/ dbengine disk space = 2048/g' /etc/netdata/netdata.conf
+sed -i 's/# x-frame-options response header =/ x-frame-options response header = SAMEORIGIN/g' /etc/netdata/netdata.conf
+systemctl restart netdata
