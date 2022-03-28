c94113a7909deb251c4b5d49e15d7e801d14ac01
Chalut Dumrongsiri <jomzsg@hotmail.com>
2022-03-28 15:48:22 +0700

    Update tuning_ubuntu_parameter.sh

--- tuning_ubuntu_parameter.sh
+++ tuning_ubuntu_parameter.sh
@@ -10,3 +10,10 @@ ulimit -SHn 65535
 # sysctl tuning
 curl -o /etc/sysctl.conf https://raw.githubusercontent.com/chalut01/setup/master/template/sysctl.conf
 sysctl -e -p /etc/sysctl.conf
+
+# clear cache crontab
+mkdir -p /opt/stack/monitor
+curl -o /opt/stack/monitor/clearcache.sh https://raw.githubusercontent.com/chalut01/setup/master/template/clearcache.sh
+chmod 755 /opt/stack/monitor/clearcache.sh
+echo "0 0 * * * /opt/stack/monitor/clearcache.sh" >> /tmp/clearcache
+crontab /tmp/clearcache
