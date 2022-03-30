736ca9f61d500c441588943003cd9b274461a55d
Chalut Dumrongsiri <jomzsg@hotmail.com>
2022-03-30 13:21:07 +0700

    Update install_docker_ubuntu18.sh

--- install_docker_ubuntu18.sh
+++ install_docker_ubuntu18.sh
@@ -9,3 +9,5 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bi
 apt update
 apt-cache policy docker-ce
 apt install -y docker-ce
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
